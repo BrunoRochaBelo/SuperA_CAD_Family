66b19b65d2274821e3cc651937c622ee362dd96e
BrunoRochaBelo <bruno.r.belo@hotmail.com>
2025-04-17 02:45:21 -0300

    Mais um teste.

--- run.py
+++ run.py
@@ -1,5 +1,7 @@
-﻿from formaturas_app import create_app
+﻿from run import app  # Puxa a instância já criada com create_app()
 
-app = create_app()
+if __name__ == "__main__":
+    app.run()
 
+application = app
     
